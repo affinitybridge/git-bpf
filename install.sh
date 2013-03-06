8e3b8a95d5b5f4f0ecfb7d9ef2f33fa675b4ccd2
tnightingale <tom@tnightingale.com>
2013-03-06 15:01:49 -0800

    added chance to back out before script executes

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR_NAME="ab-git-scripts"
+
 SOURCE_URL="https://github.com/affinitybridge/git-scripts.git"
 SOURCE_DIR="${HOME}/.${SCRIPT_DIR_NAME}"
 SOURCE_CONTEXT="--work-tree=${SOURCE_DIR} --git-dir=${SOURCE_DIR}/.git"
@@ -10,6 +11,15 @@ TARGET_DIR="${TARGET_REPO}/.git/${SCRIPT_DIR_NAME}"
 TARGET_CONTEXT="--work-tree=${TARGET_REPO} --git-dir=${TARGET_REPO}/.git"
 
 COMMAND_RECREATE_BRANCH="ruby .git/${SCRIPT_DIR_NAME}/bpf.rb recreate-branch"
+
+echo "If this is the first time running this script a copy of the AB git scripts repo (${SOURCE_URL}) will be cloned into your home directory (${SOURCE_DIR})."
+echo "If you already have a copy of the scripts in your home directory, we'll make sure it's up to date."
+echo "The directory will then be sym-linked into the target repository (${TARGET_REPO}) and local git aliases will be created for each command."
+read -p "Do you want to continue? " -n 1
+if [[ ! ${REPLY} =~ ^[Yy]$ ]]
+then
+  exit 1
+fi
 
 if [ ! -d ${SOURCE_DIR} ]; then
   # Install local copy of scripts to home dir.
