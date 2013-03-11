fe7513343de41269cdea1c651d228c43ffd087e4
tnightingale <tom@tnightingale.com>
2013-03-11 13:53:08 -0700

    added homebrew formula

---
+++ git-bpf-formula.rb
@@ -0,0 +1,7 @@
+require 'formula'
+
+class GitiBPFInit < GithubGistFormula
+    url 'https://raw.github.com/affinitybridge/git-scripts/v0.0.1/git-bpf-init'
+    homepage 'https://github.com/affinitybridge/git-scripts'
+    sha1 'a06e6f5fa1795172bd7ae672dcfaaeca9936322b'
+end
