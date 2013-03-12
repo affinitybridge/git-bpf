3532246d34d4a5bddc3cc188e3d3dd149752c290
tnightingale <tom@tnightingale.com>
2013-03-11 17:49:51 -0700

    improved rerere sharing

--- hooks/post-commit.rb
+++ hooks/post-commit.rb
@@ -6,9 +6,12 @@
 # the very thing we want to deal with post-merge is updating our rerere repo. So here we
 # do a check to find out if the last commit was a merge commit. There may be a better way
 # to check this but I don't know what it is.
-last = `git log --pretty=format:%h -1 HEAD`.chomp
-last_merge = `git log --pretty=format:%h --merges -1 HEAD`.chomp
-if last == last_merge
+parents = `git rev-list -n 1 --parents HEAD`.split("\s")
+
+# Shift off the last commit.
+last = parents.shift
+
+if parents.length >= 2
   `git share-rerere pull`
   `git share-rerere push`
 end
