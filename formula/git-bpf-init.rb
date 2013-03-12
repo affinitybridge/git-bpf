9a6b8f8f1101c503e46f5bc81fce1b4327a3c970
tnightingale <tom@tnightingale.com>
2013-03-11 18:03:33 -0700

    added head version to formula

--- formula/git-bpf-init.rb
+++ formula/git-bpf-init.rb
@@ -2,6 +2,7 @@ require 'formula'
 
 class GitBpfInit < GithubGistFormula
     url 'https://raw.github.com/affinitybridge/git-scripts/v0.0.1/git-bpf-init'
+    head 'https://raw.github.com/affinitybridge/git-scripts/master/git-bpf-init'
     homepage 'https://github.com/affinitybridge/git-scripts'
     sha1 '57c85003d6003551529d6f5301d042c68c258336'
 end
